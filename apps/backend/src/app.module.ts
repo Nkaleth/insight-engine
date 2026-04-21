455a8dcaf262a15447190089d53f619c58ec2a7c
Nkaleth <nseguralu@gmail.com>
2026-04-20 20:13:59 -0500

    Set up Z to validate en .env with envSchema

--- apps/backend/src/app.module.ts
+++ apps/backend/src/app.module.ts
@@ -1,8 +1,13 @@
 import { Module } from '@nestjs/common';
+import { ConfigModule } from '@nestjs/config';
+import { validate } from './config/config.schema';
 
 @Module({
-  imports: [],
+  imports: [ConfigModule.forRoot({
+    validate,
+    isGlobal: true
+  })],
   controllers: [],
   providers: [],
 })
-export class AppModule {}
+export class AppModule { }
